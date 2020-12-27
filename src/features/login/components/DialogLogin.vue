b19794a062c029f3e6ac23de4e723b07c7fcdeff
Yok-1337 <satthabut.o@gmail.com>
2020-12-28 00:52:42 +0700

    - [feat] add login components

---
+++ src/features/login/components/DialogLogin.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ diaLogHeader }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <div v-if="isLoginForm">
+              <login-editor-form v-model="loginForm"></login-editor-form>
+              <v-btn class="mb-3" block color="success">
+                Login
+              </v-btn>
+
+              <v-btn outlined block @click="closeDialog">
+                Close
+              </v-btn>
+            </div>
+            <div v-else>
+              <register-editor-form></register-editor-form>
+            </div>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { LoginForm } from "../forms/LoginForm";
+import LoginEditorForm from "./LoginEditorForm.vue";
+import RegisterEditorForm from "./RegisterEditorForm.vue";
+export default Vue.extend({
+  props: {
+    dialog: {
+      type: Boolean,
+    },
+  },
+  components: {
+    LoginEditorForm,
+    RegisterEditorForm,
+  },
+  data() {
+    const diaLogHeader = "Login";
+    const isLoginForm = true;
+    const loginForm: LoginForm = {};
+
+    return { diaLogHeader, isLoginForm, loginForm };
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("closeDialog");
+    },
+    test() {
+      console.log("tes");
+    },
+  },
+});
+</script>
+<style lang="scss" scoped></style>
---
+++ src/features/login/components/LoginEditorForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-row>
+      <v-text-field
+        label="Username"
+        outlined
+        dense
+      ></v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field
+        label="Passowrd"
+        outlined
+        dense
+      ></v-text-field>
+    </v-row>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { LoginForm } from "../forms/LoginForm";
+export default Vue.extend({
+  props: {
+    value: {
+      type: Object as () => LoginForm,
+    },
+  },
+});
+</script>
+<style lang="scss" scoped></style>
