e3a4bfda2a4de87aa2bc02410d21971d7e01981e
Yok-1337 <satthabut.o@gmail.com>
2020-12-27 19:46:01 +0700

    - update route and add component

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,10 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <app-bar></app-bar>
+  </v-app>
 </template>
-
 <script lang="ts">
 import Vue from "vue";
-
 export default Vue.extend({});
 </script>
+<style lang="scss" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import * as components from "@/commons/components";
+
+Object.keys(components).forEach((key) => {
+  Vue.component(key, (components as any)[key]);
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
