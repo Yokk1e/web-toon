dad6f7d85896af4c19324cc1c200fff15899b220
Yok-1337 <satthabut.o@gmail.com>
2020-12-31 11:42:28 +0700

    - [feat] update client

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -1,7 +1,9 @@
 import { HTTPClient } from "../commons/HTTPClient";
 import { AuthClient } from "./AuthClient";
+import { UserClient } from "./UserClient";
 export class Client {
   constructor(private httpClient: HTTPClient) {}
-  
+
   public auths: AuthClient = new AuthClient(this.httpClient);
+  public users: UserClient = new UserClient(this.httpClient);
 }
