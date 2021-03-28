d2a41445984b3d386b1164220bbaf87a3c7e67b2
Yok-1337 <satthabut.o@gmail.com>
2021-03-29 00:52:35 +0700

    - [feat] add editor form component

---
+++ src/features/auths/components/LoginEditorForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-row>
+      <v-text-field
+        v-model="email"
+        label="E-mail"
+        outlined
+        dense
+      ></v-text-field>
+    </v-row>
+    <v-row>
+      <v-text-field
+        v-model="password"
+        label="Passowrd"
+        outlined
+        dense
+      ></v-text-field>
+    </v-row>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { proxyModel } from "@/commons/utils/proxyModel";
+import { AuthLoginForm } from "../forms/AuthLoginForm";
+export default Vue.extend({
+  props: {
+    value: {
+      type: Object as () => AuthLoginForm,
+    },
+  },
+  computed: {
+    ...proxyModel("email", "password"),
+  },
+});
+</script>
+<style lang="scss" scoped></style>
