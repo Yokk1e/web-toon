872b56ba9c846d1b24466844e2977ae6474b991a
Yok-1337 <satthabut.o@gmail.com>
2021-03-29 00:51:56 +0700

    - [feat] update appbar and router

--- src/features/auths/AuthRouter.ts
+++ src/features/auths/AuthRouter.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import { RouteConfig } from "vue-router";
 
-import AuthLoginPage from "./pages/AuthLoginPage.vue";
+import AuthLoginPage from "../auths/pages/AuthLoginPage.vue";
 
 export const AuthRouter: (prefix?: string) => RouteConfig[] = (prefix = "") => [
   {
