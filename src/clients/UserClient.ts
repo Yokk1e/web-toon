eb6d44271f2c393dfd545bda5173a9f3e7e20537
Yok-1337 <satthabut.o@gmail.com>
2021-03-30 03:19:19 +0700

    - [feat] update user client

--- src/clients/UserClient.ts
+++ src/clients/UserClient.ts
@@ -1,9 +1,19 @@
 import { HTTPClient } from "@/commons/HTTPClient";
+import { PaginationRequestParams } from "@/commons/mixins/pagination";
 
 export interface UserRegisterRequestParams {
   email: string;
   password: string;
   userName: string;
+}
+
+export interface UserUpdateRequestParams {
+  id: number;
+  role: number;
+}
+
+export interface UserDeleteRequestParams {
+  id: number;
 }
 
 export class UserClient {
@@ -13,6 +23,45 @@ export class UserClient {
       url: "/users",
       method: "POST",
       data: params,
+    });
+
+    return data;
+  }
+
+  public async getUser(id: number): Promise<any> {
+    const { data } = await this.httpClient.request({
+      url: `/users/${id}`,
+      method: "GET"
+    });
+
+    return data;
+  }
+
+  public async getUsers(params: PaginationRequestParams): Promise<any> {
+    const { data } = await this.httpClient.request({
+      url: "/users",
+      method: "GET",
+      params
+    });
+
+    return data;
+  }
+
+  public async patchUsers(params: UserUpdateRequestParams): Promise<any> {
+    const { id, ...body } = params;
+    const { data } = await this.httpClient.request({
+      url: `/users/${id}`,
+      method: "PATCH",
+      data: body
+    });
+
+    return data;
+  }
+
+  public async deleteUsers(params: UserDeleteRequestParams): Promise<any> {
+    const { data } = await this.httpClient.request({
+      url: `/users/${params.id}`,
+      method: "DELETE"
     });
 
     return data;
