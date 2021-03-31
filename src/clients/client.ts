e8ddf8db344a1e102bac1800caea423086d46854
Yok-1337 <satthabut.o@gmail.com>
2021-03-31 19:53:16 +0700

    - [feat] add role client

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -1,9 +1,11 @@
 import { HTTPClient } from "../commons/HTTPClient";
 import { AuthClient } from "./AuthClient";
 import { UserClient } from "./UserClient";
+import { RoleClient } from "./RoleClient";
 export class Client {
   constructor(private httpClient: HTTPClient) {}
 
   public auths: AuthClient = new AuthClient(this.httpClient);
   public users: UserClient = new UserClient(this.httpClient);
+  public roles: RoleClient = new RoleClient(this.httpClient);
 }
